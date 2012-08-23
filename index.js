8916115533672436503f0c05a2664ed3f8f105e5
TJ Holowaychuk <tj@vision-media.ca>
2012-08-22 19:00:49 -0700

    add test.html

--- index.js
+++ index.js
@@ -8,12 +8,14 @@
  */
 
 module.exports = function(canvas){
-  var ratio = window.devicePixelRatio || 1;
-  canvas.style.width = canvas.width;
-  canvas.style.height = canvas.height;
-  canvas.width *= ratio;
-  canvas.height *= ratio;
   var ctx = canvas.getContext('2d');
-  ctx.scale(ratio, ratio);
+  var ratio = window.devicePixelRatio || 1;
+  if (1 != ratio) {
+    canvas.style.width = canvas.width;
+    canvas.style.height = canvas.height;
+    canvas.width *= ratio;
+    canvas.height *= ratio;
+    ctx.scale(ratio, ratio);
+  }
   return canvas;
 };
